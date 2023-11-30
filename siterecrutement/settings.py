7ad9383319fb9a015aa982a6bd5401cf6a423d42
oussama chaouki pc <oussama11chaouki@gmail.com>
2023-11-30 13:12:38 +0100

    adding picture config

--- siterecrutement/settings.py
+++ siterecrutement/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+AUTH_USER_MODEL = 'recruter.User'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -122,9 +125,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
