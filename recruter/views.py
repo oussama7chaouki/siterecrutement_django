75f3d2ced226b055ab1cd8bbe44ead670525cb5c
oussama chaouki pc <oussama11chaouki@gmail.com>
2023-11-30 10:30:54 +0100

    dynamique navbar

--- recruter/views.py
+++ recruter/views.py
@@ -12,7 +12,7 @@ def offer(request):
  offers = [
     {"id_job": 1, "job_title": "Software Engineer", "domain": "Information Technology", "date": "2023-11-28"},
     {"id_job": 2, "job_title": "Financial Analyst", "domain": "Finance", "date": "2023-11-25"},
-    {"id_job": 3, "job_title": "Marketing Manager", "domain": "Marketing", "date": "2023-11-20"},
+    {"id_job": 3, "job_title": "Marketing Manscorer", "domain": "Marketing", "date": "2023-11-20"},
     ]
  context = {'offers': offers}
  return render(request, 'recruter/jobs.html',context)
@@ -35,5 +35,10 @@ def jobcans(request):
  context = {'jobcans': jobcan}
  return render(request, 'recruter/jobcans.html',context)
 def jobcan(request,pk):
- 
- return render(request, 'recruter/jobcan.html')
+   datas = [
+      {'name': 'Alice', 'score': 25, 'email': 'alice@example.com'},
+      {'name': 'Bob', 'score': 30, 'email': 'bob@example.com'},
+      {'name': 'Charlie', 'score': 22, 'email': 'charlie@example.com'},
+   ]
+   context = {'datas': datas}
+   return render(request, 'recruter/jobcan.html',context)
